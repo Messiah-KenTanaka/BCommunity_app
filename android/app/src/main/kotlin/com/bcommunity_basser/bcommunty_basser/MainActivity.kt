f7e4d80197a5e6dddbd4b4eb9b4759c6030b990b
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-05-05 16:31:50 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/bcommunity_basser/bcommunty_basser/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bcommunity_basser.bcommunty_basser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BASSER',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'BASSER'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const WebView(
+        initialUrl: 'https://www.bcommunity-basser.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
